e0edf2cad0c3f02ceb3fbb08459a839cce118967
rohit-raj7 <rohitraj70615@gmail.com>
2025-08-31 19:58:41 +0530

    sd

--- my/app/interview/DataResume.js
+++ my/app/interview/DataResume.js
@@ -4,10 +4,8 @@ export async function uploadResume(resumeFile) {
   const formData = new FormData();
   formData.append('resume', resumeFile);
  
-  // const api = 'https://pyhon-backend.onrender.com'
-  //          || 'https://interview-now1-1.onrender.com';
-
- const api='https://pyhon-backend.onrender.com'
+  const api = 'https://pyhon-backend.onrender.com'
+           || 'https://interview-now1-1.onrender.com'; 
 
   const res = await fetch(`${api}/upload`, {
     method: 'POST',
